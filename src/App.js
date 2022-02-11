a309f6779d4a0cd3aaf575d9b5241a68bef03ba4
DavidGalileo24 <david.salgado.mejia@gmail.com>
2022-02-10 22:59:53 -0600

    Agregando rutas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './ssets/App.css';
+//librarys
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {BrowserRouter, Route, Routes, Link} from 'react-router-dom'
+//pages
+import Login from './views/Login/Login';
+import Clientes from './views/Clientes/Clientes';
+import Vehiculos from './views/Vehiculos/Vehiculos';
+import Inicio from './views/Inicio';
+import Navbar from './templates/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+        {/*Routes*/}
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Inicio/>} ></Route>
+                <Route exact path="/inicio-de-sesion" element={<Login/>} ></Route>
+                <Route exact path="/clientes" element={<Clientes/>} ></Route>
+                <Route exact path="/recuperar-contrasenia" element={<RecuperarContrasenia/>} ></Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
